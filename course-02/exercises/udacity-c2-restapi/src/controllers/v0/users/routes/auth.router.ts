55449fcf245fe6187ba7bb5155fdedce09e15730
Steven Duncan-Brown <stevedb@users.noreply.github.com>
2020-04-24 02:42:58 +0100

    Latest stable dev merged to master (#4)
    * Update config and implement some basic functions as part of lesson 4 (#1)
    
    * Feature/auth (#2)
    
    * Adds bcrypt types as a dev dependancy
    
    * Implements functions to generate and compare passwords in the auth router
    
    * remove old hardcoded values from config file - now env vars
    
    * Tidy up the generate and compare password code
    
    * Adds hardcoded jwt secret the config file and implements the generate jwt function
    
    * unstub requireAuth middleware function
    
    * Make JWT secret an env variable
    
    * Adds new script to package.json to deploy app to EB
    
    * Fix issue in generate JWT function
    
    * Adds apiHost value pointing to Udagram instance running in AWS
    
    * Feature/image filter service (#3)
    
    * Adds npm script to deploy service to EB
    
    * update the util -> filterImageFromURL function to deal with image urls that don't return an image
    
    * Implements the GET /filteredimage?image_url={{}} endpoint in the server.ts file
    
    * Update http status code to 422 for when an image is unavailable
    
    * adds type for output path in the util -> filterImageFromURL function
    
    * Adds screenshot of running EB deployment dashboard

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -7,41 +7,44 @@ import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
+import { config } from '../../../../config/config';
 
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+
+    return bcrypt.hash(plainTextPassword, salt);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    return bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
+    return jwt.sign(user.toJSON(), config.dev.jwt.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
-    
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
-    
-    // const token = token_bearer[1];
 
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    const token_bearer = req.headers.authorization.split(' ');
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+
+    const token = token_bearer[1];
+
+    return jwt.verify(token, config.dev.jwt.secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
 }
 
 router.get('/verification', 
